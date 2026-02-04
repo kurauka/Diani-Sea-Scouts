96804a9867cf1d16804630bf238dfe21d3c3c5a8
Mohamed Abdalla Mohamed <58578637+kurauka@users.noreply.github.com>
2026-02-04 13:06:24 +0300

    Delete includes directory

--- includes/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="bg-brand-dark text-white text-center p-4 mt-auto">
-    <p>&copy;
-        <?php echo date("Y"); ?> Diani Sea Scouts. All Rights Reserved.
-    </p>
-</footer>
-</body>
-
-</html>
